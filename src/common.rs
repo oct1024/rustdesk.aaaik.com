1bc2cd6e368f8afa362152f1d2a755b9b25195d8
oct1024 <97272873+oct1024@users.noreply.github.com>
2025-05-23 01:13:30 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -993,7 +993,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rustdesk.aaaik.com:21114".to_owned()
 }
 
 #[inline]
